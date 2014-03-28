fb17087b1cb8a7b5f7738f5e3c736b5a8c4f6166
Ron Crans <ron@rnp-web.net>
2014-03-28 12:59:36 -0400

    phase 2

--- com_usersched/site/views/usersched/tmpl/sv_default_error.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('_JEXEC') or die;
-
-if ($this->error)
-	echo '<div class="error">'.$this->escape($this->error).'</div>';
-?>
--- com_usersched/site/views/usersched/tmpl/sv_default_nope.php
+++
@@ -1,4 +0,0 @@
-<?php
-//echo'<xmp>';var_dump($this->user->groups,$this->params->get('site_auth'));echo'</xmp>';
-?>
-THIS CALENDAR IS NOT ACCESSIBLE
