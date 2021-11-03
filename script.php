cd7d7156ee5ad6d95d21aec881dd7f7b6fbf26d3
Ron Crans <ron@rjconline.net>
2021-11-03 13:52:39 +0000

    improved installer script

--- script.php
+++ script.php
@@ -1,13 +1,18 @@
 <?php
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Installer\InstallerScript;
 use Joomla\CMS\Log\Log;
 
-class com_userschedInstallerScript
+class com_userschedInstallerScript extends InstallerScript
 {
+	protected $minimumJoomla = '3.8';
+	protected $com_name = 'com_usersched';
+
 	public function install ($parent) 
 	{
-		$parent->getParent()->setRedirectURL('index.php?option=com_usersched');
+		$parent->getParent()->setRedirectURL('index.php?option='.$this->com_name);
 	}
 
 	public function uninstall ($parent) 
@@ -20,11 +25,16 @@ class com_userschedInstallerScript
 
 	public function preflight ($type, $parent) 
 	{
+		// give the parent first shot
+		if (parent::preflight($type, $parent) === false) return false;
+
+		// ensure that SQLite is active in joomla
 		$dbs = JDatabaseDriver::getConnectors();
 		if (!in_array('sqlite', $dbs) && !in_array('Sqlite', $dbs)) {
 			Log::add('Joomla support for SQLite(3) is required for this component.', Log::WARNING, 'jerror');
 			return false;
 		}
+		// get the version number being installed/updated
 		if (method_exists($parent,'getManifest')) {
 			$this->release = $parent->getManifest()->version;
 		} else {
@@ -35,25 +45,25 @@ class com_userschedInstallerScript
 	public function postflight ($type, $parent) 
 	{
 		$params['version'] = $this->release;
-		$this->setParams($params, true);
+		$this->mySetParams($params, true);
 		if ($type == 'install') {
-			$params['user_canskin'] = '0';
-			$params['user_canalert'] = '0';
-			$params['user_recurrevt'] = '0';
-			$params['grp_canskin'] = '0';
-			$params['grp_canalert'] = '0';
-			$params['grp_recurrevt'] = '0';
-			$params['show_versions'] = '1';
-			$this->setParams($params);
+			$params['user_canskin'] = false;
+			$params['user_canalert'] = false;
+			$params['user_recurrevt'] = false;
+			$params['grp_canskin'] = false;
+			$params['grp_canalert'] = false;
+			$params['grp_recurrevt'] = false;
+			$params['show_versions'] = true;
+			$this->mySetParams($params);
 		}
 	}
 
-	private function setParams ($param_array, $replace=false)
+	private function mySetParams ($param_array, $replace=false)
 	{
 		if (count($param_array) > 0) {
 			// read the existing component value(s)
 			$db = JFactory::getDbo();
-			$db->setQuery('SELECT params FROM #__extensions WHERE name = "com_usersched"');
+			$db->setQuery('SELECT params FROM #__extensions WHERE name = "'.$this->com_name.'"');
 			$params = json_decode($db->loadResult(), true);
 			// add the new variable(s) to the existing one(s), replacing existing only if requested
 			foreach ($param_array as $name => $value) {
@@ -62,7 +72,7 @@ class com_userschedInstallerScript
 			}
 			// store the combined new and existing values back as a JSON string
 			$paramsString = json_encode($params);
-			$db->setQuery('UPDATE #__extensions SET params = ' . $db->quote($paramsString) . ' WHERE name = "com_usersched"');
+			$db->setQuery('UPDATE #__extensions SET params = ' . $db->quote($paramsString) . ' WHERE name = "'.$this->com_name.'"');
 			$db->execute();
 		}
 	}
